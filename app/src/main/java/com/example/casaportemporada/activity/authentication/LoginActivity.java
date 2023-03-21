17f1253cff4a8fed0d875d41c5fbc730967d4519
luis_bandeira <luis.bandeira@wisereducacao.com>
2023-03-21 08:32:12 -0300

    feat: initial config of firebase and login activity

---
+++ app/src/main/java/com/example/casaportemporada/activity/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.casaportemporada.activity.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.casaportemporada.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(com.example.casaportemporada.R.layout.activity_login);
+    }
+}
